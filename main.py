fe8594cd09a3ca0d1bd2701219974b75129593a4
elabs <elijahknsubuga@gmail.com>
2021-10-08 11:55:18 +0300

    fixed bug on line 5

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import instaloader
 
 ig = instaloader.Instaloader()
 profile = input("Enter Username: ")
-ig.downloader_profile(profile, profile_pic_only = True)
+ig.download_profile(profile, profile_pic_only = True)
